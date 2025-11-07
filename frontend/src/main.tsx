1e00dd09e6ab4d5b0242d8f812261b18872b7e10
PhinehasBerko <phinehasdscience@gmail.com>
2025-11-07 23:22:55 +0000

    improve SEO, disable previous App.tsx with it's configurations and update with TrackerApp in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
+import TrackerApp from "./TrackerApp";
+// import App from "./App.tsx";
+// import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,8 +12,9 @@ if (!PUBLISHABLE_KEY) {
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <TrackerApp />
+    {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY}> */}
+      {/* <App /> */}
+    {/* </ClerkProvider> */}
   </React.StrictMode>
 );
