895207578e6df69089d7d6564f4cd4e4cd40e8c1
PhinehasBerko <phinehasdscience@gmail.com>
2026-06-16 09:46:25 +0000

    Refactor Express application to  Meteor with project setup

---
+++ client/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Meteor } from "meteor/meteor"
+import { createRoot } from "react-dom/client";
+import { App } from "../imports/ui/App";
+import "./main.css"
+
+
+Meteor.startup(() => {
+  const container = document.getElementById("react-target");
+
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  }
+});
