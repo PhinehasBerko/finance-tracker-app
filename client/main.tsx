49294f04d59cb7f74e63fe1ed714d6de0283238c
PhinehasBerko <phinehasdscience@gmail.com>
2026-06-30 23:39:23 +0000

    fix: cssconfiguration issues

--- client/main.tsx
+++ client/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Meteor } from "meteor/meteor"
 import { createRoot } from "react-dom/client";
 import { App } from "../imports/ui/App";
-import "./main.css"
+import './main.css'
 
 
 Meteor.startup(() => {
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Income from './Income';
 export const App = () => (
   <div className="max-w-3xl min-h-screen mx-auto sm:pt-10">
    <h1>Track, Manage and Visualize every penny</h1>
+   <Income/>
   </div>
 );
