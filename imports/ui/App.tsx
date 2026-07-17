71df655ed2a4663c4d5095a9665f2973587d8470
PhinehasBerko <phinehasdscience@gmail.com>
2026-07-17 23:32:43 +0000

    fix(client) login route and narrow type definition for react-target container element

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import Income from './Income';
-export const App = () => (
-  <div className="max-w-3xl min-h-screen mx-auto sm:pt-10">
-   <h1>Track, Manage and Visualize every penny</h1>
-   <Income/>
-  </div>
-);
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+
+import { Login } from './Login';
+import Dashboard from './Dashboard';
+
+export const App = () => {
+  const {user, isLoading } = useTracker(() => ({
+    user: Meteor.user(),
+    isLoading: Meteor.loggingIn(),
+  }),[]);
+
+  if (isLoading) {
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        <svg className="animate-spin h-8 w-8 text-blue-500" viewBox="0 0 24 24" fill="none">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
+        </svg>
+      </div>
+    )
+  }
+  if (!user) {
+    return <Login />
+  }
+
+  return <Dashboard />
+};
