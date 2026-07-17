cd960e86c5c8fcf7616ee3399b1ab4b0feea924c
PhinehasBerko <phinehasdscience@gmail.com>
2026-07-17 23:34:14 +0000

    fix(client) login route and narrow type definition for react-target container element

--- client/main.tsx
+++ client/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Meteor } from "meteor/meteor"
-import { createRoot } from "react-dom/client";
+import {createRoot } from "react-dom/client";
 import { App } from "../imports/ui/App";
-import './main.css'
-
+import './main'
+import '/imports/api/income/methods';
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
-
-  if (container) {
-    const root = createRoot(container);
-    root.render(<App />);
+  if (!container) {
+    throw new Error("Failed to find the root element with id 'react-target'");
   }
+
+  const root = createRoot(container);
+ 
+    root.render(<App />);
+
 });
