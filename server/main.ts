a019e72fe0c9315166b09f1cb706b662901f13de
PhinehasBerko <phinehasdscience@gmail.com>
2026-07-31 23:10:52 +0000

    register the methods and publications and the server

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,11 @@
 import { Meteor } from "meteor/meteor";
-import "./../imports/api/register-collections";
 
-Meteor.startup(() => {
+import "./../imports/api/register-collections";
+import '/imports/api/income/methods';
+import '/imports/api/expenses/methods';
+import 'imports/api/expenses/publications';
+import '/imports/api/income/publications';
+
+Meteor.startup(async() => {
     console.log("server running")
 })
